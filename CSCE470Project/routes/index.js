76f3b086f8b4d036c110e1a1288dd2cad3cab5d7
cameronbrock <cameronbrock@tamu.edu>
2019-11-29 00:31:54 -0600

    Testing Solr communication.

--- CSCE470Project/routes/index.js
+++ CSCE470Project/routes/index.js
@@ -1,15 +1,25 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
+var fetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
-router.post('/', function (req, res) {
-    console.log(req.body);
-    res.send(formatData(req));
+router.post('/results/', function (req, res) {
+    // Solr URL used to send requests to the API.
+    var numRows = 10;
+    var url = 'http://localhost:8983/solr/moviedb/select?q=genres.name%3A' + req.body['genre'] + '&rows=' + numRows + '&start=0';
+
+    fetch(url)
+        .then((response) => {
+            response.text().then((solrResponse) => {
+                res.render('results', { results: solrResponse });
+            });
+        });
+
 });
 
 /* Example function on how to access what is passed from the form in index.pug */
