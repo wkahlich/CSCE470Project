ac56b27c57981652c56220622d64eec28f89dd00
cameronbrock <cameronbrock@tamu.edu>
2019-11-30 22:28:45 -0600

    Testing API query construction.

--- CSCE470Project/routes/index.js
+++ CSCE470Project/routes/index.js
@@ -11,7 +11,9 @@ router.get('/', function (req, res) {
 router.post('/results/', function (req, res) {
     // Solr URL used to send requests to the API.
     var numRows = 10;
-    var url = 'http://localhost:8983/solr/moviedb/select?q=genres.name%3A' + req.body['genre'] + '&rows=' + numRows + '&start=0';
+    //var url = 'http://localhost:8983/solr/moviedb/select?q=genres.name%3A' + req.body['genre'] + '&rows=' + numRows + '&start=0';
+    var url = getURL(req.body['genre'], 'No', 'Recent', 'English Speaking', 'Yes', 'Yes', 'No', numRows);
+    console.log('URL: ' + url);
 
     fetch(url)
         .then((response) => {
@@ -21,6 +23,50 @@ router.post('/results/', function (req, res) {
         });
 
 });
+
+function getURL(q1, q2, q3, q4, q5, q6, q7, numRows) {
+    // What is your favorite genre?
+    var url = 'http://localhost:8983/solr/moviedb/select?q=genres.name%3A' + q1;
+
+    // Do ratings of movies impact your choise of movie?
+    if (q2 == 'Yes') {
+        url += ' AND popularity%3A[5 TO 10]'
+    }
+
+    // Do you prefer recent releases or old movies?
+    if (q3 == 'Recent') {
+        url += ' AND release_date%3A[2000-01-01T00%3A00%3A00Z TO 2020-01-01T00%3A00%3A00Z]';
+    }
+    else if (q3 == 'Old') {
+        url += ' AND release_date%3A[1900-01-01T00%3A00%3A00Z TO 2000-01-01T00%3A00%3A00Z]';
+    }
+
+    // Do you prefer English speaking movies or International?
+    if (q4 == 'English Speaking') {
+        url += ' AND original_language%3Aen';
+    }
+    else if (q4 == 'International') {
+        url += ' AND NOT original_language%3Aen';
+    }
+
+    // Do you watch animated movies?
+    if (q5 == 'No') {
+        url += 'AND NOT genres.name%3AAnimation';
+    }
+
+    // Do you watch family/kids movies?
+    if (q6 == 'No') {
+        url += ' AND NOT genres.name%3AFamily';
+    }
+
+    // Do you/Would you watch/enjoy TV movies?
+    if (q7 == 'No') {
+        url += ' AND NOT genres.name%3A"TV Movie"'
+    }
+
+    url += '&rows=' + numRows + '&start=0';
+    return url;
+}
 
 /* Example function on how to access what is passed from the form in index.pug */
 function formatData(req) {
