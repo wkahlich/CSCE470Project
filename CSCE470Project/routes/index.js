7f3becee6aac3af239261ec37445444a540cbbb0
w-kahlich <w_kahlich@tamu.edu>
2019-12-02 19:12:01 -0600

    changed url for my machine - revert for yours

--- CSCE470Project/routes/index.js
+++ CSCE470Project/routes/index.js
@@ -28,42 +28,42 @@ router.post('/results/', function (req, res) {
 
 function getURL(q1, q2, q3, q4, q5, q6, q7, numRows) {
     // 1) What is your favorite genre?
-    var url = 'http://localhost:8983/solr/moviedb/select?q=genres.name%3A' + q1;
+    var url = 'http://localhost:8983/solr/movies/select?q=genres.name%3A' + q1;
 
     // 2) Do ratings of movies impact your choise of movie?
-    if (q2 == 'Yes') {
-        url += ' AND popularity%3A[5 TO 10]'
+    if (q2 === 'Yes') {
+        url += ' AND popularity%3A[5 TO 10]';
     }
 
     // 3) Do you prefer recent releases or old movies?
-    if (q3 == 'Recent') {
+    if (q3 === 'Recent') {
         url += ' AND release_date%3A[2000-01-01T00%3A00%3A00Z TO 2020-01-01T00%3A00%3A00Z]';
     }
-    else if (q3 == 'Old') {
+    else if (q3 === 'Old') {
         url += ' AND release_date%3A[1900-01-01T00%3A00%3A00Z TO 2000-01-01T00%3A00%3A00Z]';
     }
 
     // 4) Do you prefer English speaking movies or International?
-    if (q4 == 'English Speaking') {
+    if (q4 === 'English Speaking') {
         url += ' AND original_language%3Aen';
     }
-    else if (q4 == 'International') {
+    else if (q4 === 'International') {
         url += ' AND NOT original_language%3Aen';
     }
 
     // 5) Do you watch animated movies?
-    if (q5 == 'No') {
+    if (q5 === 'No') {
         url += ' AND NOT genres.name%3AAnimation';
     }
 
     // 6) Do you watch family/kids movies?
-    if (q6 == 'No') {
+    if (q6 === 'No') {
         url += ' AND NOT genres.name%3AFamily';
     }
 
     // 7) Do you/Would you watch/enjoy TV movies?
-    if (q7 == 'No') {
-        url += ' AND NOT genres.name%3A"TV Movie"'
+    if (q7 === 'No') {
+        url += ' AND NOT genres.name%3A"TV Movie"';
     }
 
     url += '&rows=' + numRows + '&start=0';
